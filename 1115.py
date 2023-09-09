62ea01c65d0dbcc7184d81d58e3da36bb47f4a45
rafaelguasselli <rafael.g.l.silva@gmail.com>
2023-09-08 22:45:03 -0300

    ex 1115

---
+++ 1115.py
@@ -0,0 +1,15 @@
+def quadrante(x, y):
+    if (x == 0 or y == 0): 
+        return 
+    if (x > 0  and y > 0):  
+        print("primeiro")
+    if (x <= 0 and y > 0):  
+        print("segundo")
+    if (x <= 0 and y <= 0):  
+        print("terceiro")   
+    if (x > 0  and y <= 0):  
+        print("quarto")
+
+while True: 
+    x, y = map(int, input().split())
+    quadrante(x, y)

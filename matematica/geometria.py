0e1ed6905e1134c90af5663e92bddc2e269c6f69
rafaelGuasselli <rafael.g.l.silva@gmail.com>
2023-11-01 17:44:26 -0300

    lista dicionarios

--- matematica/geometria.py
+++ matematica/geometria.py
@@ -1,7 +1,7 @@
 def calculateAreaFromClockWiseCoordinates(coordinates):
 	area = 0
 	for i in range(0, len(coordinates)):
-		currentI, nextI = i, i+1 % len(coordinates)
+		currentI, nextI = i, (i + 1) % len(coordinates)
 		cy, cx = coordinates[currentI]
 		ny, nx = coordinates[nextI]
 		area += (cx * ny - nx * cy)

49e353f911e7d2a6e7ab112476abd4ed652231b0
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-03-16 23:40:01 -0300

    a

--- vjudge/lista_2_adhock/b.py
+++ vjudge/lista_2_adhock/b.py
@@ -1,6 +1,9 @@
 def inverter(direcao):
 	return (direcao + 1) % 2
 
+def naoEstaVazia(lista):
+    return len(lista) > 0
+
 qtdPessoas = int(input())
 pessoas = []
 
@@ -14,24 +17,22 @@ TEMPO_MOVIMENTO = 10
 tempoAtual, direcaoAtual = pessoas[0]
 pessoasEsperando = [[], []]
 
-while len(pessoas + pessoasEsperando[0] + pessoasEsperando[1]) > 0:
-	aindaTemPessoas = lambda: len(pessoas) > 0
+while naoEstaVazia(pessoas + pessoasEsperando[0] + pessoasEsperando[1]):
 	aPessoaJaChegou = lambda: pessoas[0][0] <= tempoAtual
 	faltaPessoasEsperando = lambda: len(pessoasEsperando[0] + pessoasEsperando[1]) == 0
 	
-	while aindaTemPessoas() and (aPessoaJaChegou() or faltaPessoasEsperando()):
+	while naoEstaVazia(pessoas) and (aPessoaJaChegou() or faltaPessoasEsperando()):
 		pulaTempo = not aPessoaJaChegou() and faltaPessoasEsperando()
 		proximoTempo, proximaDirecao = proximaPessoa = pessoas.pop(0)
 		pessoasEsperando[proximaDirecao].append(proximaPessoa)
 		if pulaTempo: tempoAtual = proximoTempo
 	
-	vaiPraMesmaDirecao = not len(pessoasEsperando[direcaoAtual]) == 0
+	vaiPraMesmaDirecao = naoEstaVazia(pessoasEsperando[direcaoAtual])
 	if vaiPraMesmaDirecao:
 		tempo, direcao = pessoasEsperando[direcaoAtual].pop(0)
 		tempoAtual = tempo + TEMPO_MOVIMENTO
 	else:
 		direcaoAtual = inverter(direcaoAtual)
-		tempo, direcao = pessoasEsperando[direcaoAtual].pop(0)
 		tempoAtual = tempoAtual + TEMPO_MOVIMENTO
 		pessoasEsperando[direcaoAtual] = []
   

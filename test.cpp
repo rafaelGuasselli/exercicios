b90942db8426c516c9a4ac257e5c70c352d925fa
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-04-26 20:29:03 -0300

    c

--- test.cpp
+++ test.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 
 int main() {
-    set<int> a;
-	a.insert(1);
-	a.insert(2);
-	a.insert(3);
-	a.insert(10);
-	a.insert(12);
-	a.insert(20);
+				if (m >= price[i] && bestFavor(m - price[i], i - 1) + favor[i] > bestFavor(m, i - 1) && (m <= money || (bestSpent(m - price[i], i - 1) + price[i]) > 2000)) {
+					dp[m][i] =  bestFavor(m - price[i], i - 1) + favor[i];
+					spent[m][i] = bestSpent(m - price[i], i - 1) + price[i];
+				} else {
+					dp[m][i] = bestFavor(m, i - 1);
+					spent[m][i] = bestSpent(m, i - 1);
+				}
 
 	cout<<*a.lower_bound(-12031)<<endl;
 	cout<<*prev(a.lower_bound(-12031), 1)<<endl;

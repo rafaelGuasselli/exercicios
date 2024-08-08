be6bee62a5087fc9e5931ab14f0c2f5700c2cf99
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-08-08 07:15:36 -0300

    2017

---
+++ obi/progSenior/2017/cortandoPapel.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n; scanf("%d", &n);
+	vector<int> nums(n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &nums[n]);
+	}
+
+
+	//20 5 15 5 22
+	vector<int> peaks;
+	for (int i = 0; i < n; i++) {
+
+	}
+
+	return 0;
+}

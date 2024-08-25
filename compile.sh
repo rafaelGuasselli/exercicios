e47936e1747d5a1a4616945d8b671814be5ecc01
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-08-25 09:09:25 -0300

    as

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
 rm -f $1.exe 
-if (g++ -o $1.exe $1.cpp -Wall -pedantic -lm -g) then 
+if (g++ -o a.exe $1.cpp -Wall -pedantic -lm -g) then 
 	echo "### COMPILOU ###" 
-	if !(./$1.exe  < $1.in > $1.out 2>&1) then 
+	if !(time ./a.exe  < $1.in > $1.out 2>&1) then 
 		echo "### RUNTIME ERROR ###" >> $1.out 
 	fi 
 fi

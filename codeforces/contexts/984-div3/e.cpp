d79d4e7086db70cd11b89594d2e0685a21559d85
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-11-02 16:36:01 -0300

    e

--- codeforces/contexts/984-div3/e.cpp
+++ codeforces/contexts/984-div3/e.cpp
@@ -1,11 +1,10 @@
 #include<bits/stdc++.h>
-#define int long long
+//#define int long long
 using namespace std;
 
 
-vector<vector<int>> matrix;
-vector<vector<int>> maior;
-vector<vector<int>> menor;
+int matrix[100005];
+int regions[100005];
 
 
 signed main() {
@@ -14,41 +13,27 @@ signed main() {
 	ios_base::sync_with_stdio(0);
 
 	int n, k, q; cin>>n>>k>>q;
-	maior.assign(k, {});
-	menor.assign(k, {});
 	
 	for (int i = 0; i < n; i++) {
-		matrix.push_back({});
 		for (int j = 0; j < k; j++) {
-			int val; cin>>val;
-			matrix[i].push_back(val);
+			cin>>matrix[i*k+j];
 		}
 	}
 
 	for (int i = 0; i < k; i++) {
-		// maior[0].push_back(matrix[i][0]);
-		// menor[0].push_back(-matrix[i][0]);
+		regions[i*n] = matrix[i];
 
 		for (int j = 1; j < n; j++) {
-			matrix[j][i] = matrix[j][i] | matrix[j-1][i];
-			// maior[j].push_back(matrix[i][j]);
-			// menor[j].push_back(-matrix[i][j]);
+			matrix[j*k+i] = matrix[j*k+i] | matrix[(j-1)*k+i];
+			regions[i*n+j] = matrix[j*k+i];
 		}
 	}
 
-	cout<<"a"<<endl;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < k; j++) {
-			cout<<matrix[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-	cout<<endl;
 
 	for (int i = 0; i < q; i++) {
 		int r; cin>>r;
 		int menorInd = 0;
-		int maiorInd = n-1;
+		int maiorInd = n;
 
 		for (int j = 0; j < r; j++) {
 			int ki; cin>>ki; ki--;
@@ -56,15 +41,15 @@ signed main() {
 			int c; cin>>c;
 
 			if (op == '<') {
-				int ind = upper_bound(menor[ki].begin(), menor[ki].end(), -c)-menor[ki].begin();
-				maiorInd = min(maiorInd, ind);
+				int ind = lower_bound(regions + (ki*n), regions + (ki*n+n), c) - (regions + (ki*n));
+				maiorInd = min(maiorInd, ind-1);
 			} else {
-				int ind = upper_bound(maior[ki].begin(), maior[ki].end(), c)-maior[ki].begin();
+				int ind = upper_bound(regions + (ki*n), regions + (ki*n+n), c) - (regions + (ki*n));
 				menorInd = max(menorInd, ind);
 			}
 		}
 
-		if (menorInd <= maiorInd) {
+		if (menorInd <= maiorInd && menorInd < n) {
 			cout<<menorInd+1<<endl;
 		} else {
 			cout<<-1<<endl;

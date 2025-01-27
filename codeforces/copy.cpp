73e4be67bd3d08e91fa80ce3cb2e499afdf75985
Rafael Guasselli <rafael.g.l.silva@gmail.com>
2025-01-27 01:24:46 -0300

    f

--- codeforces/copy.cpp
+++ codeforces/copy.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
-#define max(a, b) a > b ? a : b 
-#define min(a, b) a > b ? b : a 
+#define max(a, b) (a > b ? a : b) 
+#define min(a, b) (a > b ? b : a)
 //#define int long long
 using namespace std;
 
@@ -9,5 +9,6 @@ signed main() {
 	cout.tie(0);
 	ios_base::sync_with_stdio(0);
 	
+
 	return 0;
 }

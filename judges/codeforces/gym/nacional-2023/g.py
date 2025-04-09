91fc792461297c1235784110d626501b77f7545b
rafaelguasselli <rafael.g.l.silva@gmail.com>
2025-04-09 15:10:05 -0300

    g 2.0

--- judges/codeforces/gym/nacional-2023/g.py
+++ judges/codeforces/gym/nacional-2023/g.py
@@ -5,10 +5,10 @@ def intersect(retaA, retaB):
 	lxc, rxc = max(lxa, lxb), min(rxa, rxb)
 	if lxc > rxc:
 		return None
-	
+
 	if typeA == typeB == "reta":
-		if a != c and b != d:
-			x = (d-b)//(a-c)
+		if a != c:
+			x = (d - b) // (a - c)
 			y = x * a + b
 			if x >= lxc and x <= rxc:
 				return ("ponto", x, y, x, x)
@@ -16,18 +16,18 @@ def intersect(retaA, retaB):
 			return ("reta", a, b, lxc, rxc)
 	elif typeA == typeB == "ponto" and a == c and b == d:
 		return retaA
-	elif typeA == "ponto" and typeB == "reta" and a*c+d == b:
-		return ("ponto", a, b, a, a) 
-	elif typeA == "reta" and typeB == "ponto" and c*a+b == d:
-		return ("ponto", c, d, c, c) 
+	elif typeA == "ponto" and typeB == "reta" and a * c + d == b:
+		return ("ponto", a, b, a, a)
+	elif typeA == "reta" and typeB == "ponto" and c * a + b == d:
+		return ("ponto", c, d, c, c)
 	return None
 
 def construirRetas(a, b, c):
 	retas = []
-	retas.append(("reta",  1, b + c - a, a - c,		a))
-	retas.append(("reta", -1, a + b + c, 	a,	a + c))
-	retas.append(("reta",  1, b - a - c, 	a,	a + c))
-	retas.append(("reta", -1, a + b - c, a - c, 	a))
+	retas.append(("reta",  1, b + c - a, a - c,     a))
+	retas.append(("reta", -1, a + b + c,     a,    a + c))
+	retas.append(("reta",  1, b - a - c,     a,    a + c))
+	retas.append(("reta", -1, a + b - c, a - c,     a))
 	return retas
 
 def construirPontos(reta):
@@ -42,21 +42,20 @@ def construirPontos(reta):
 		return pontos
 	else:
 		return []
-	
 
 n = int(input())
 a, b, c = map(int, input().split())
-retas = construirRetas(a, b, c)
-for i in range(n-1):
+retas = set(construirRetas(a, b, c))
+for _ in range(n-1):
 	a, b, c = map(int, input().split())
-	novasRetas = construirRetas(a, b, c)
-	interRetas = []
+	novasRetas = set(construirRetas(a, b, c))
+	interRetas = set()
 
-	for i in retas:
-		for j in novasRetas:
-			inter = intersect(i, j)
+	for r in retas:
+		for nr in novasRetas:
+			inter = intersect(r, nr)
 			if inter:
-				interRetas.append(inter)
+				interRetas.add(inter)
 	retas = interRetas
 
 pontos = []

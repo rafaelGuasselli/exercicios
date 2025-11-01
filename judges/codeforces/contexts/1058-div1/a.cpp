11a5746a6eb935fdf971a44c90fee5b9a1ce94a1
rafaelguasselli <rafael.g.l.silva@gmail.com>
2025-11-01 17:41:40 -0300

    01/11/25

---
+++ judges/codeforces/contexts/1058-div1/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+vi solution;
+void solve(int l, int r) {
+
+}	
+
+
+signed main() {
+	cout.tie(0);
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
+
+	int t; cin>>t;
+	while (t--) {
+		int n; cin>>n;
+		solution.clear();
+		solution.assign(2*n, 0);
+	}
+}

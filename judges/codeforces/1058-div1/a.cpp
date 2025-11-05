e1086b5f67c5baf5f8967e324cf3d437a6802919
rafaelguasselli <rafael.g.l.silva@gmail.com>
2025-11-05 11:14:14 -0300

    05/11/25

--- judges/codeforces/1058-div1/a.cpp
+++ judges/codeforces/1058-div1/a.cpp
@@ -7,8 +7,6 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-map<set<int>, int> memo;
-vector<pair<int, int>> range;
 
 
 #ifdef DEBUG
@@ -22,8 +20,9 @@ ostream dcout(&null_buffer);
 
 int n; 
 vi solution;
-set<int> removePosition;
-vi temp = {1,2,3,1,2,3};
+bitset<100000> mark;
+vi temp = {1,2,1,2};
+vector<pair<int, int>> ind;
 
 
 int big(vi positions){
@@ -38,32 +37,11 @@ int big(vi positions){
 	return biggest;
 } 
 
-int biggestValueOnIndexes(vi positions) {
-	vi search;
-	for (int i: positions) {
-		if (removePosition.find(i) == removePosition.end()) {
-			search.push_back(i);
-		}
-	}
-
+int biggestValueOnIndexes(vi search) {
 	if (search.size() <= 1) {
 		return 0;
 	}
 
-	set<int> vals, vals2;
-	for (int i: search) {vals.insert(i);}
-	for (int i: positions) {vals2.insert(i);}
-
-	if (memo.find(vals) != memo.end()) {
-		dcout<<"result: "<<memo[vals]<<endl;
-		return memo[vals];
-	}	
-
-	if (memo.find(vals2) != memo.end() && memo[vals2] == 0) {
-		dcout<<"result: "<<memo[vals2]<<endl;
-		return memo[vals2];
-	}	
-	
 	stringstream output;
 	output<<"? "<<search.size()<<" ";
 	for (int i: search) {
@@ -81,9 +59,8 @@ int biggestValueOnIndexes(vi positions) {
 #else
 	cin>>biggest;
 #endif
-	dcout<<"result: "<<biggest<<endl;
-
-	memo[vals] = biggest;
+	
+	dcout<<"result: "<<biggest<<endl;	
 	return biggest;
 }
 
@@ -109,115 +86,69 @@ void printSolutionDebug() {
 	dcout.flush();
 }
 
-vi generateRangeInclusive(int l, int r) {
-	vi result;
-	for (int i = l; i <= r; i++) {
-		result.push_back(i);
-	}
-	return result;
-}
-
-pair<int, int> findTheSplit(int l, int r, int val) {
-	int m = l+(r-l)/2;
-	
-	while (1) {
-		vi left = generateRangeInclusive(l, m);
-		vi right = generateRangeInclusive(m+1, r);
-
-		int lRes = biggestValueOnIndexes(left);
-		if (lRes != 0) {
-			range[lRes] = {max(range[lRes].first, l), min(range[lRes].second, m)};
-		}
-
-		if (lRes == val) {
-			r = m;
-			m = l+(r-l)/2;
-			continue;
-		}
-
-		int rRes = biggestValueOnIndexes(right);
-		if (rRes != 0) {
-			range[rRes] = {max(range[rRes].first, l), min(range[rRes].second, m)};
-		}
-
-		if (lRes != val && rRes != val) {
-			return {l, r};
-		} else {
-			l = m+1;
-			m = l+(r-l)/2;
-		}
-	}
-}
-
-int findTheIndex(int val, int l0, int r0, vi half) {
-	int r  = r0;
-	int l = l0;
-	int m = l+(r-l)/2;
-	
-	while (1) {
-		if (l == r) {
-			return l;
-		}
-
-		dcout<<"index: "<<l<<" "<<r<<endl;
-		vi left = generateRangeInclusive(l, m);
-		for (int i: half) {
-			left.push_back(i);
-		}
-
-		int lRes = biggestValueOnIndexes(left);
-		if (lRes == val) {
-			r = m;
-			m = l+(r-l)/2;
-		} else {
-			l = m+1;
-			m = l+(r-l)/2;
-		}
-	}
-}
-
-// 1 2 3 4 5 6
-// 1 2 3 1 2 3
-void solve(int val) {
-	auto [ml, mr] = range[val];
-	auto [l, r]= findTheSplit(ml, mr, val);
-	int m = l+(r-l)/2;
-
-	dcout<<"split: "<<l<<" "<<r<<endl;
-	int i1 = findTheIndex(val, l, m, generateRangeInclusive(m+1, r));
-	int i2 = findTheIndex(val, m+1, r, {i1});
-
-	solution[i1] = val;
-	solution[i2] = val;
-
-	removePosition.insert(i1);
-	removePosition.insert(i2);
-
-	printSolutionDebug();
-}		
-
-
-//2 2 1 1
 signed main() {
 	cout.tie(0);
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
-
+		
 	int t; cin>>t;
 	while (t--) {
 		cin>>n;
 		solution.clear();
 		solution.assign(2*n, 0);
-		removePosition.clear();
-		memo.clear();
-		range.clear();
-		range.assign(n+1, {});
-		for (int i = 0; i <= n; i++) {
-			range[i] = {0, 2*n-1};
+		
+		mark.reset();
+		ind.clear();
+		ind.assign(n+1, {});
+		
+		vi current = {0, 1};
+		while (current[1] < 2*n) {
+			int biggest = biggestValueOnIndexes(current); 
+			
+			if (biggest == 0) {
+				break;
+			} else {
+				ind[biggest].first = current[0];
+				ind[biggest].second = current[1];
+				solution[current[0]] = biggest;
+				solution[current[1]] = biggest;
+				mark.set(current[0]);
+				mark.set(current[1]);
+				current = {current[0]+2, current[1]+2};
+			}	
 		}
 
-		for (int i = n; i >= 1; i--) {
-			solve(i);
+		for (int i = current[1]+1; i < 2*n; i++) {
+			current.push_back(i);
+			int biggest = biggestValueOnIndexes(current);
+			if (biggest != 0) {
+				ind[biggest].first = i;
+				solution[i] = biggest;
+
+				mark.set(i);
+				current.pop_back();
+			}
+		}
+
+		current.clear();
+		for (int i = 1; i <= n; i++) {
+			if (ind[i].second == 0) {
+				current.push_back(ind[i].first);
+			}
+		}
+
+		for (int i = 0; i < 2*n; i++) {
+			if (mark.test(i)) {
+				continue;
+			}
+
+			current.push_back(i);
+			int biggest = biggestValueOnIndexes(current);
+			if (biggest != 0) {
+				ind[biggest].second = i;
+				solution[i] = biggest;
+				current.pop_back();
+			}
 		}
 
 		printSolution();
